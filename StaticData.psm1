8a57b846720cc38ab4f85f87dc2447c0e9f4dcee
kittholland <kittholland@gmail.com>
2015-07-20 10:05:23 -0700

    Additional Parameters for static data
    Added name and ID parameters for querying static data for specific
    champions, items, runes, masteries, summoner spells.

--- StaticData.psm1
+++ StaticData.psm1
@@ -1,10 +1,14 @@
 ﻿Function Get-StaticChampionData
 {
-    [CmdletBinding()]
+    [CmdletBinding(DefaultParameterSetName='None')]
     Param
     (
         [ValidateSet('br', 'eune', 'euw', 'kr', 'lan', 'las', 'na', 'oce', 'ru', 'tr')]
-        [string]$Region = 'na'
+        [string]$Region = 'na',
+        [Parameter(ParameterSetName='Name')]
+        [string[]]$Name,
+        [Parameter(ParameterSetName='ID')]
+        [string[]]$ID
     )
     $results = @()
     $entries = Invoke-RiotRestMethod -BaseUri "https://$Region.api.pvp.net/api/lol/static-data/$Region/v1.2/champion"
@@ -12,21 +16,61 @@
     {
         $results += $entries.data.$entry
     }
-    $results
+    If($PSCmdlet.ParameterSetName -eq 'Name')
+    {
+        Foreach($item in $name)
+        {
+            $results | Where-Object {$_.name -eq $item}
+        }
+    }
+    ElseIf($PSCmdlet.ParameterSetName -eq 'ID')
+    {
+        Foreach($item in $id)
+        {
+            $results | Where-Object {$_.id -eq $item}
+        }
+    }
+    Else
+    {
+        $results
+    }
 }
 
 Function Get-StaticItemData
 {
-    [CmdletBinding()]
+    [CmdletBinding(DefaultParameterSetName='None')]
     Param
     (
         [ValidateSet('br', 'eune', 'euw', 'kr', 'lan', 'las', 'na', 'oce', 'ru', 'tr')]
-        [string]$Region = 'na'
+        [string]$Region = 'na',
+        [Parameter(ParameterSetName='Name')]
+        [string[]]$Name,
+        [Parameter(ParameterSetName='ID')]
+        [string[]]$ID
     )
+    $results = @()
     $entries = Invoke-RiotRestMethod -BaseUri "https://$Region.api.pvp.net/api/lol/static-data/$Region/v1.2/item"
     Foreach ($entry in ($entries.data | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name))
     {
-        $entries.data.$entry
+        $results += $entries.data.$entry
+    }
+    If($PSCmdlet.ParameterSetName -eq 'Name')
+    {
+        Foreach($item in $name)
+        {
+            $results | Where-Object {$_.name -eq $item}
+        }
+    }
+    ElseIf($PSCmdlet.ParameterSetName -eq 'ID')
+    {
+        Foreach($item in $id)
+        {
+            $results | Where-Object {$_.id -eq $item}
+        }
+    }
+    Else
+    {
+        $results
     }
 }
 
@@ -69,16 +113,39 @@ Function Get-StaticMapData
 
 Function Get-StaticMasteryData
 {
-    [CmdletBinding()]
+    [CmdletBinding(DefaultParameterSetName='None')]
     Param
     (
         [ValidateSet('br', 'eune', 'euw', 'kr', 'lan', 'las', 'na', 'oce', 'ru', 'tr')]
-        [string]$Region = 'na'
+        [string]$Region = 'na',
+        [Parameter(ParameterSetName='Name')]
+        [string[]]$Name,
+        [Parameter(ParameterSetName='ID')]
+        [string[]]$ID
     )
+    $results = @()
     $entries = Invoke-RiotRestMethod -BaseUri "https://$Region.api.pvp.net/api/lol/static-data/$Region/v1.2/mastery"
     Foreach ($entry in ($entries.data | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name))
     {
-        $entries.data.$entry
+        $results += $entries.data.$entry
+    }
+    If($PSCmdlet.ParameterSetName -eq 'Name')
+    {
+        Foreach($item in $name)
+        {
+            $results | Where-Object {$_.name -eq $item}
+        }
+    }
+    ElseIf($PSCmdlet.ParameterSetName -eq 'ID')
+    {
+        Foreach($item in $id)
+        {
+            $results | Where-Object {$_.id -eq $item}
+        }
+    }
+    Else
+    {
+        $results
     }
 }
 
@@ -95,31 +162,77 @@ Function Get-StaticRealmData
 
 Function Get-StaticRuneData
 {
-    [CmdletBinding()]
+    [CmdletBinding(DefaultParameterSetName='None')]
     Param
     (
         [ValidateSet('br', 'eune', 'euw', 'kr', 'lan', 'las', 'na', 'oce', 'ru', 'tr')]
-        [string]$Region = 'na'
+        [string]$Region = 'na',
+        [Parameter(ParameterSetName='Name')]
+        [string[]]$Name,
+        [Parameter(ParameterSetName='ID')]
+        [string[]]$ID
     )
+    $results = @()
     $entries = Invoke-RiotRestMethod -BaseUri "https://$Region.api.pvp.net/api/lol/static-data/$Region/v1.2/rune"
     Foreach ($entry in ($entries.data | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name))
     {
-        $entries.data.$entry
+        $results += $entries.data.$entry
+    }
+    If($PSCmdlet.ParameterSetName -eq 'Name')
+    {
+        Foreach($item in $name)
+        {
+            $results | Where-Object {$_.name -eq $item}
+        }
+    }
+    ElseIf($PSCmdlet.ParameterSetName -eq 'ID')
+    {
+        Foreach($item in $id)
+        {
+            $results | Where-Object {$_.id -eq $item}
+        }
+    }
+    Else
+    {
+        $results
     }
 }
 
 Function Get-StaticSummonerSpellData
 {
-    [CmdletBinding()]
+    [CmdletBinding(DefaultParameterSetName='None')]
     Param
     (
         [ValidateSet('br', 'eune', 'euw', 'kr', 'lan', 'las', 'na', 'oce', 'ru', 'tr')]
-        [string]$Region = 'na'
+        [string]$Region = 'na',
+        [Parameter(ParameterSetName='Name')]
+        [string[]]$Name,
+        [Parameter(ParameterSetName='ID')]
+        [string[]]$ID
     )
+    $results = @()
     $entries = Invoke-RiotRestMethod -BaseUri "https://$Region.api.pvp.net/api/lol/static-data/$Region/v1.2/summoner-spell"
     Foreach ($entry in ($entries.data | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name))
     {
-        $entries.data.$entry
+        $results += $entries.data.$entry
+    }
+    If($PSCmdlet.ParameterSetName -eq 'Name')
+    {
+        Foreach($item in $name)
+        {
+            $results | Where-Object {$_.name -eq $item}
+        }
+    }
+    ElseIf($PSCmdlet.ParameterSetName -eq 'ID')
+    {
+        Foreach($item in $id)
+        {
+            $results | Where-Object {$_.id -eq $item}
+        }
+    }
+    Else
+    {
+        $results
     }
 }
 
